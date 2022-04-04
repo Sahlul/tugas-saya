b1e6f1eeac354894e020231ffd26b26b0447fb2b
Sahlul <102942195+Sahlul@users.noreply.github.com>
2022-04-04 22:20:37 +0700

    tugas pertama java

---
+++ tugas_pertama_java.java
@@ -0,0 +1,26 @@
+class Main {
+    public static void main(String[] args) {
+        String nama, tanggalLahir, jenisKelamin, golDarah, asalKampus, tempatTinggal, job, statKawin, agama;
+        nama = "Kris April Mas Sahlul";
+        tanggalLahir = "26 April 2000";
+        jenisKelamin = "Laki-laki";
+        golDarah = "O";
+        asalKampus = "Universitas Muhammadiyah Sumatera Utara";
+        tempatTinggal = "Medan";
+        job = "Mahasiswa";
+        statKawin = "belum menikah";
+        agama = "islam";
+
+        System.out.println("Nama : " + nama);
+        System.out.println("Tanggal Lahir : " + tanggalLahir);
+        System.out.println("Jenis Kelamin : " + jenisKelamin);
+        System.out.println("Golongan Darah : " + golDarah);
+        System.out.println("Asal Kampus : " + asalKampus);
+        System.out.println("Tempat Tinggal : " + tempatTinggal);
+        System.out.println("Pekerjaan : " + job);
+        System.out.println("Status Kawin : " + statKawin);
+        System.out.println("Agama : " + agama);
+        
+    }
+    
+}
